e3f0cd5e96e0b905ac0cc7835d5b97466eb6fbd0
Zuum <djzuumik@gmail.com>
2016-04-17 22:54:26 +0300

    Scene setup

---
+++ Assets/CaveController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveController : MonoBehaviour {
+	
+	int state;
+	int color;
+	int[] workersNeed = { 0, 0, 0, 0 };
+	public Sprite[] stateSprites;
+	public SpriteRenderer stateSprite;
+	// Use this for initialization
+	void Start () {
+		state = 0;
+		stateSprite.sprite = stateSprites [state];
+		System.Random rand = new System.Random ();
+		for (int i = 0; i < 4; i++) {
+			workersNeed[i] = rand.Next (4);
+		}
+		color = rand.Next (3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
